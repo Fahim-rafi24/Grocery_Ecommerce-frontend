f089ceaed28fe58f10928644c2cdfbf8724bcd12
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-12-05 14:48:06 +0600

    install react-helmet & sweetalert2

--- src/Components/Pages/Search.jsx
+++ src/Components/Pages/Search.jsx
@@ -1,9 +1,14 @@
+import { useParams } from "react-router-dom";
 
 
 const Search =() =>{
+    const {search} = useParams();
+    const hastag = {hastag: search}
+    console.log(hastag)
     return(
         <div className="w-full p-3 border">
         Search
+        {search}
         </div>
     );
 }

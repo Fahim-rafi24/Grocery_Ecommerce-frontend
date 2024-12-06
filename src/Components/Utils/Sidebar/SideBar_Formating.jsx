259e1329bd5593e4db73f648be66ce54b9bd347b
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-12-06 09:16:09 +0600

    add (login & register) file & also add some new photos in assets

--- src/Components/Utils/Sidebar/SideBar_Formating.jsx
+++ src/Components/Utils/Sidebar/SideBar_Formating.jsx
@@ -1,6 +1,6 @@
 // json data
 import sidebarData from "../../../../public/sidebar_root.json"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 
 
@@ -36,34 +36,34 @@ const SideBar_Formating = () => {
                     :
                     // Industry list
                     sidebarData.map((Industry, inx) => <div key={inx}>
-                        <Link to={`/Industry/${Industry?.Industry}`} className="flex flex-row my-2 hover:text-red-400">
+                        <NavLink to={`/Industry/${Industry?.Industry}`} className="flex flex-row my-2 hover:text-red-400">
                             <img src={Industry?.pic} alt={Industry?.Industry} className="h-5 w-5" />
                             <p>{Industry?.Industry.toUpperCase()}</p>
-                        </Link>
+                        </NavLink>
                         {/* Catagory list*/}
                         {
                             <div className="border-l border-gray-500 dark:border-gray-600 font-normal ml-2">
                                 {
                                     Industry?.All_Catagory.map((Catagory, inx) => <div key={inx}>
-                                        <Link
+                                        <NavLink
                                             to={`/Catagory/${Catagory_params(Industry?.Industry, Catagory?.Catagory)}`}
                                             className="flex flex-row my-2 ml-2 hover:text-blue-400">
                                             {`${inx + 1}.`}
                                             <img src={Catagory?.pic} alt={Catagory?.Catagory} className="h-5 w-5 ml-2" />
                                             <p className="ml-2">{Catagory?.Catagory.toUpperCase()}</p>
-                                        </Link>
+                                        </NavLink>
                                         {/* SubCatagory list*/}
                                         <>
                                             <div className="border-l border-gray-500 dark:border-gray-600 ml-5">
                                                 {
                                                     Catagory?.All_SubCatagory.map((SubCatagory, inx) => <div key={inx}>
-                                                        <Link
+                                                        <NavLink
                                                             to={`/SubCatagory/${SubCatagory_params(Industry?.Industry, Catagory?.Catagory, SubCatagory?.SubCatagory)}`}
                                                             className="flex flex-row my-2 ml-2 hover:text-green-400">
                                                             {/*  */}
                                                             <img src={SubCatagory?.pic} alt={SubCatagory?.SubCatagory} className="h-5 w-5" />
                                                             <p className="ml-2">{SubCatagory?.SubCatagory.toUpperCase()}</p>
-                                                        </Link>
+                                                        </NavLink>
                                                     </div>)
                                                 }
                                             </div>

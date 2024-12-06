188f2ea1b4f8b616e6052f15f0e7a9387c76021b
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-12-06 22:17:46 +0600

    set firebaseAuth key

--- src/ContextStorage/UserContext.jsx
+++ src/ContextStorage/UserContext.jsx
@@ -11,14 +11,13 @@ const UserProvider = ({ children }) => {
     const callUser = {
         "_id": 300,
         name: 'kazi rafi',
-        avater: 'https://chaldn.com/_mpimage/popular?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D95790&q=best&v=1&m=40&webp=1&alpha=1',
+        // avater: 'https://chaldn.com/_mpimage/popular?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D95790&q=best&v=1&m=40&webp=1&alpha=1',
         Permanent_location: "Vakutiya, jessore, Khulna, Bangladesh",
         current_location: "Vakutiya, jessore, Khulna, Bangladesh",
         Mobile_NO: "+8801784918086",
         isAdmin : true,
         email: 'kazirafibd@gmail.com'
     }
-
     // call user
     useEffect(() => {
         if (!user) {

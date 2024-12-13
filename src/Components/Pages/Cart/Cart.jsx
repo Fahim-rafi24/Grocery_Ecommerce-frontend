4ee1d664982368c1ef78179e27c834f75af698d3
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-12-13 12:28:07 +0600

    user previous order status cart added

--- src/Components/Pages/Cart/Cart.jsx
+++ src/Components/Pages/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import { HelmetFunc } from "../../Utils/Helmet/Helmet";
 import { storyStatus } from "../../Utils/storyStatus/storyStatus";
 import { ItemContext } from "../../../ContextStorage/ItemContext";
 import axios_without_cookies from "../../../Axios/axios_without_cookies";
+import axios_with_cookies from "../../../Axios/axios_with_cookies.jsx";
 import ProductCard from "../../Utils/ProductCard/ProductCard.jsx";
 import { UserContext } from "../../../ContextStorage/UserContext.jsx";
 import { useNavigate } from "react-router-dom";
@@ -16,6 +17,8 @@ const Cart = () => {
     const navigate = useNavigate();
 
     // cart state
+    const [previousOrders, setOrders] = useState(null);
+    // console.log(previousOrders);
     const [cartList, setCartList] = useState([]);
     const [totalCost, setCostTotal] = useState(0);
     useEffect(() => {
@@ -25,6 +28,16 @@ const Cart = () => {
         }
         callAPI();
     }, [])
+
+    useEffect(() => {
+        if (user?._id && !previousOrders) {
+            const callAPI = async () => {
+                const response = await axios_with_cookies.post("/order_status", { id: user?._id, obj: {} })
+                setOrders(response.data.data);
+            }
+            callAPI();
+        }
+    }, [user])
 
     useEffect(() => {
         // call total cost api
@@ -59,19 +72,23 @@ const Cart = () => {
                 }
             });
         } else {
-            Swal.fire({
-                title: "Are you sure?",
-                text: `Your Total Cost of Product is ${totalCost} TK.`,
-                icon: "info",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Yes Do it!"
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    navigate(`/payment/${user._id}`, { state: { totalCost } }) 
-                }
-            });
+            if (totalCost > 0) {
+                Swal.fire({
+                    title: "Are you sure?",
+                    text: `Your Total Cost of Product is ${totalCost} TK.`,
+                    icon: "info",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes Do it!"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        navigate(`/payment/${user._id}`, { state: { totalCost } })
+                    }
+                });
+            } else {
+                Swal.fire("Order First!");
+            }
         }
     }
 
@@ -101,6 +118,40 @@ const Cart = () => {
                                 <ProductCard obj={each}></ProductCard>
                             </div>
                         ))
+                }
+            </section>
+
+            {/* display old order list */}
+            <section>
+                {
+                    previousOrders ? <>
+                        {
+                            previousOrders.map((eachOrder, inx) => <div key={inx}>
+                                {/* top section */}
+                                <div className={`border-t dark:border-none text-center md:text-start p-5 shadow-2xl my-10 ${eachOrder?.order_status === "Delivery" ? "bg-green-400 dark:text-black": ""}   ${eachOrder?.order_status === "Cancel" ? "bg-red-400 dark:text-black" : ""}`}>
+                                    <div className="md:flex justify-between">
+                                        <p className="text-2xl font-semibold">Total Pay : {eachOrder?.payAmount}</p>
+                                        <p>Order Status : <span className="font-bold">{eachOrder?.order_status}</span></p>
+                                    </div>
+                                    <div className="md:flex justify-between pb-4 border-b">
+                                        <p>User Name : {eachOrder?.userDetails?.name}</p>
+                                        <p>User Email : {eachOrder?.userDetails?.email}</p>
+                                    </div>
+                                    {
+                                        eachOrder?.order_products.map(order_products => <div className="flex justify-between my-4">
+                                            <img src={order_products.product.img} alt="" className="h-20" />
+                                            <div className="text-end">
+                                                <p>Each Item Price : {order_products?.product?.name}</p>
+                                                <p>Each Item Price : {order_products?.price}</p>
+                                                <p>Total Item Quantity : {order_products?.quantity}</p>
+                                            </div>
+                                        </div>)
+                                    }
+                                    <p className="text-center">order Id : {eachOrder?._id}</p>
+                                </div>
+                            </div>)
+                        }
+                    </> : ""
                 }
             </section>
 

a309b55992f74855c95a9d2cbb03c08b94892bbc
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-12-13 20:49:15 +0600

    admin order API status setup complete

--- src/Components/Pages/Admin/OrderList/OrderList.jsx
+++ src/Components/Pages/Admin/OrderList/OrderList.jsx
@@ -20,15 +20,39 @@ const OrderList = () => {
     }, [searchValue])
 
     // order_status changing button func
-    const handleChangedOrderStatus =(_id, state) =>{  // this _id defind order _id & state defind order_status
+    const handleChangedOrderStatus = (_id, state) => {  // this _id defind order _id & state defind order_status
         if (!_id) {
             Swal.fire({
-                title : "Try Again ..."
+                title: "Try Again ..."
             })
             return
         }
-        console.log(_id);  // order _id
-        console.log(state);  // set this state in order order_status
+        Swal.fire({
+            title: "Are you sure?",
+            text: `You ${state} this Order!`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const callAPI = async () => {
+                    const response = await axios_with_cookies.post("/OrderStatusChanged", { id: user._id, obj: { state, _id } })
+                    if (response.data.data.status === "success") {
+                        Swal.fire({
+                            title: `${state} Complete..`
+                        })
+                        const callAPI2 = async () => {  // recall api
+                            const response = await axios_with_cookies.post("/Order_admin_status", { id: user?._id, obj: { searchValue } })
+                            setOrders(response.data.data);
+                        }
+                        callAPI2();
+                    }
+                }
+                callAPI();
+            }
+        });
     }
 
     // user info alart func
@@ -72,7 +96,7 @@ const OrderList = () => {
                 {
                     orders ? <div className="px-5">
                         {
-                            orders.map((eachOrder, inx) =>  (<div key={inx}>
+                            orders.map((eachOrder, inx) => (<div key={inx}>
                                 {/* top section */}
                                 <div className={`border-t dark:border-none text-center md:text-start p-5 shadow-2xl my-10 ${eachOrder?.order_status === "Delivery" ? "bg-green-400 dark:text-black" : ""}   ${eachOrder?.order_status === "Cancel" ? "bg-red-400 dark:text-black" : ""}`}>
                                     <div className="md:flex justify-between">
@@ -93,15 +117,15 @@ const OrderList = () => {
                                             </div>
                                         </div>)
                                     }
-                                    <p className="text-center text-orange-600">order Id : - {eachOrder?._id}</p>
+                                    <p className="text-center text-orange-900">order Id : - {eachOrder?._id}</p>
                                     <div className="text-end my-5 md:mr-10">
                                         <button onClick={() => userInfoHandle(eachOrder?.userDetails)} className="btn rounded-xl bg-amber-300 dark:text-black">User Info</button>
                                     </div>
                                     <div className="md:flex justify-between">
-                                        <p className="text-green-600">Create Date : {eachOrder?.createdAt}</p>
-                                        <p className="text-purple-600">Update Date : {eachOrder?.updatedAt}</p>
+                                        <p className="text-green-900">Create Date : {eachOrder?.createdAt}</p>
+                                        <p className="text-purple-900">Update Date : {eachOrder?.updatedAt}</p>
                                     </div>
-                                    <div className="flex justify-evenly">
+                                    <div className={`flex justify-evenly ${eachOrder?.order_status === "Pending" || "hidden"}`}>
                                         <button onClick={() => handleChangedOrderStatus(eachOrder?._id, "Delivery")} className="btn rounded-xl bg-green-500 dark:text-black">Order Complete</button>
                                         <button onClick={() => handleChangedOrderStatus(eachOrder?._id, "Cancel")} className="btn rounded-xl bg-red-600 dark:text-black">Order Cancel</button>
                                     </div>
